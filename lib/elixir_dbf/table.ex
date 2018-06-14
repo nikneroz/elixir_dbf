086500c0ccf3b18baca901546369b968b16e1191
Rozenkin Denis <rozenkin@gmail.com>
2018-06-14 10:39:15 +0000

    fast fix - add `after` block

--- lib/elixir_dbf/table.ex
+++ lib/elixir_dbf/table.ex
@@ -9,30 +9,33 @@ defmodule ElixirDbf.Table do
   alias ElixirDbf.Header
 
   def read(path, encoding \\ nil) do
-    case File.binread(path) do
+    case File.open(path) do
       {:error, reason} ->
         {:error, reason}
-      {:ok, content} ->
-        {:ok, file} = StringIO.open(content)
-        header = Header.parse(file)
+      {:ok, file} ->
+        try do
+          header = Header.parse(file)
 
-        rows =
-          for _row_index <- 1..header.records do
-            row_block = IO.binread(file, header.record_size)
-            ElixirDbf.Row.parse(row_block, header.columns, header.version, encoding || header.encoding)
+          rows =
+            for _row_index <- 1..header.records do
+              row_block = IO.binread(file, header.record_size)
+              ElixirDbf.Row.parse(row_block, header.columns, header.version, encoding || header.encoding)
+            end
+
+          case IO.binread(file, 1) do
+            <<26>> -> :eof = IO.binread(file, 1)
+            :eof -> :eof
           end
 
-        case IO.binread(file, 1) do
-          <<26>> -> :eof = IO.binread(file, 1)
-          :eof -> :eof
-        end
+          records_amount = header[:records]
 
-        records_amount = header[:records]
-
-        with ^records_amount <- length(rows) do
-          {:ok, %__MODULE__{rows: rows, header: header}}
-        else
-          e -> e
+          with ^records_amount <- length(rows) do
+            {:ok, %__MODULE__{rows: rows, header: header}}
+          else
+            e -> e
+          end
+        after
+          File.close(file)
         end
     end
   end
