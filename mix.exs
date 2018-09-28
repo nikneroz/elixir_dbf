aa5e54f31c1763566c668c7d77fcad9d66ab26ac
Rozenkin Denis <rozenkin@gmail.com>
2018-09-28 19:17:07 +0300

    Bump version 0.1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirDbf.MixProject do
   def project do
     [
       app: :elixir_dbf,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "Small library for DBF parsing written in pure elixir",
