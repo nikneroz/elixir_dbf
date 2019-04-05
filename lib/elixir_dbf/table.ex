1a1b9037d21e6a88abbe8a5e3356ced1462857c8
Rozanov <rozanov61@gmail.com>
2019-04-05 17:26:05 +0300

    add read_count function

--- lib/elixir_dbf/table.ex
+++ lib/elixir_dbf/table.ex
@@ -34,9 +34,27 @@ defmodule ElixirDbf.Table do
     read_rows(file, header, encoding, next_block, [row | rows])
   end
 
-  def fetch_data(file, encoding) do
+  def read_rows_count(file, header, count, encoding, prev_block, rows \\ [])
+  def read_rows_count(_file, _header, 0, _encoding, _prev_block, rows), do: rows
+  def read_rows_count(_file, _header, _count, _encoding, :eof, rows), do: Enum.reverse(rows)
+  def read_rows_count(_file, _header, _count, _encoding, <<26>>, rows), do: Enum.reverse(rows)
+  def read_rows_count(file, header, count, encoding, :start, rows) do
+    next_block = IO.binread(file, header.record_size)
+    read_rows_count(file, header, count, encoding, next_block, rows)
+  end
+  def read_rows_count(file, header, count, encoding, prev_block, rows) do
+    row = parse_row(prev_block, header.columns, encoding || header.encoding)
+    next_block = IO.binread(file, header.record_size)
+    read_rows_count(file, header, count - 1, encoding, next_block, [row | rows])
+  end
+
+  def fetch_data(file, count \\ nil, encoding) do
     header = Header.parse(file)
-    rows = read_rows(file, header, encoding, :start)
+    rows = 
+      case count do
+        nil -> read_rows(file, header, encoding, :start)
+        count -> read_rows_count(file, header, count, encoding, :start)
+      end
     data = %__MODULE__{rows: rows, header: header}
 
     if length(rows) == header.records do
@@ -54,6 +72,15 @@ defmodule ElixirDbf.Table do
         {:error, reason}
       {:ok, file} ->
         fetch_data(file, encoding)
+    end
+  end
+
+  def read_count(path, count, encoding \\ nil) do
+    case File.open(path) do
+      {:error, reason} ->
+        {:error, reason}
+      {:ok, file} ->
+        fetch_data(file, count, encoding)
     end
   end
 end
